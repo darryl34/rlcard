50712aacb07501a46185b9ccb1b21193a4735683
Darryl <manchesterunitedfc277@gmail.com>
2024-03-18 23:03:08 +0000

    fixed env not being properly initialized

---
+++ examples/run_leduc_onebet.py
@@ -0,0 +1,54 @@
+import os
+import sys
+
+sys.path.insert(0, os.getcwd())
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+import rlcard
+from rlcard.agents import DQNAgent, RandomAgent
+from rlcard.utils import (
+    get_device,
+    set_seed,
+    tournament,
+    reorganize,
+    Logger,
+    plot_curve,
+)
+
+# Make environment
+env = rlcard.make('leduc-onebet')
+
+# set up agents
+dqn_agent = DQNAgent(num_actions=env.num_actions,
+                     state_shape=env.state_shape[0],
+                     mlp_layers=[64,64],
+                     device=get_device())
+rand_agent = RandomAgent(num_actions=env.num_actions)
+env.set_agents([dqn_agent, rand_agent])
+
+num_eps = 5000
+eval_every = 100
+eval_games = 1000
+with Logger('leduc_holdem_dqn') as logger:
+    for ep in range(num_eps):
+        # generate data from the environment
+        trajectories, payoffs = env.run(is_training=True)
+
+        # reorganize data to be state, action, reward, next_state, done
+        trajectories = reorganize(trajectories, payoffs)
+
+        # feed transitions into agent memory, and train the agent
+        # assume dqn agent always plays first
+        for ts in trajectories[0]:
+            dqn_agent.feed(ts)
+
+        # evaluate the performance. Play with random agents.
+        if ep % eval_every == 0:
+            logger.log_performance(
+                ep, 
+                tournament(env, eval_games)[0]
+            )
+
+    csv_path, fig_path = logger.csv_path, logger.fig_path
+
+plot_curve(csv_path, fig_path, 'DQN on one step Leduc Holdem')
--- rlcard/envs/leduc_onebet.py
+++ rlcard/envs/leduc_onebet.py
@@ -1,4 +1,8 @@
-from rlcard.envs.leducholdem import Env
+import json
+import os
+
+import rlcard
+from rlcard.envs.leducholdem import LeducholdemEnv as Env
 from rlcard.games.leduc_onebet import Game
 
 
@@ -8,4 +12,10 @@ class LeducOneBetEnv(Env):
         '''
         self.game = Game()
         self.name = 'leduc-onebet'
-        super().__init__(config)
+        super(Env, self).__init__(config)       # inherit from rlcard.envs.Env instead
+        self.actions = ['call', 'raise', 'fold', 'check']
+        self.state_shape = [[36] for _ in range(self.num_players)]  # TODO: we dont need this much space
+        self.action_shape = [None for _ in range(self.num_players)]
+
+        with open(os.path.join(rlcard.__path__[0], 'games/leducholdem/card2index.json'), 'r') as file:
+            self.card2index = json.load(file)
